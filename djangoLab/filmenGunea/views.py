debc1554e87b863172011cbde2e90d6c1700ec28
anderpm <6pander6@gmail.com>
2022-03-30 12:35:16 +0200

    register.html linka

--- djangoLab/filmenGunea/views.py
+++ djangoLab/filmenGunea/views.py
@@ -1,5 +1,9 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'filmenGunea/index.html')
+
+def register(request):
+    return render(request, 'filmenGunea/register.html')
