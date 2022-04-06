36de59e8b272bf07b881d12f4e47100079e198a3
AitorElorza <67389214+AitorElorza@users.noreply.github.com>
2022-04-06 12:12:55 +0200

    Delete forms.py

--- djangoLab/filmenGunea/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class RegisterForm(forms.Form):
-    user= forms.CharField(max_length=20,required=True)
-    pass1=forms.CharField(widget=forms.PasswordInput, required=True)
-    pass2=forms.CharField(widget=forms.PasswordInput, required=True)
