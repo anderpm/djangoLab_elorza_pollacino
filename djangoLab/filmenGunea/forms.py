706288a8934cc602fbeb5be822f391369e85e9ed
AitorElorza <67389214+AitorElorza@users.noreply.github.com>
2022-04-06 12:13:34 +0200

    Add files via upload

---
+++ djangoLab/filmenGunea/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class RegisterForm(forms.Form):
+    user= forms.CharField(max_length=20,required=True)
+    pass1=forms.CharField(widget=forms.PasswordInput, required=True)
+    pass2=forms.CharField(widget=forms.PasswordInput, required=True)
