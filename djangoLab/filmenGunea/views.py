56bcbb15c381d58d1ca55444004bda12976da7b7
AitorElorza <aitor.elorza.segurola@gmail.com>
2022-04-06 11:52:34 +0200

    create first form

--- djangoLab/filmenGunea/views.py
+++ djangoLab/filmenGunea/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import RegisterForm
 
 
 def index(request):
     return render(request, 'filmenGunea/index.html')
 
 def register(request):
-    return render(request, 'filmenGunea/register.html')
+    if request.method=='POST':
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            a=1
+    else:
+        form=RegisterForm()
+            
+    return render(request, 'filmenGunea/register.html',{'form':form})
 
 def login(request):
+    
     return render(request, 'filmenGunea/login.html')
 
 def filmakIkusi(request):
