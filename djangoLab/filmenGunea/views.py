ddd89fca2b66d3bd511f5ca6d6ca94802f3149c3
AitorElorza <aitor.elorza.segurola@gmail.com>
2022-04-07 16:10:21 +0200

    views back to the begining

--- djangoLab/filmenGunea/views.py
+++ djangoLab/filmenGunea/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import RegisterForm, LoginForm
-from django.contrib.auth import authenticate, login as auth_login
-from django.contrib.auth.models import User
-
 
 
 def index(request):
@@ -11,16 +8,9 @@ def index(request):
 
 def register(request):
     if request.method=='POST':
-        erabil = request.POST['user']
-        pasahitza1 = request.POST['pass1']
-        pasahitza2 = request.POST['pass2']
-        if pasahitza1==pasahitza2:
-            user = authenticate(username=erabil,password=pasahitza1)
-            if user is None:
-              us=User.objects.create_user(erabil,email=None,password= pasahitza1)
-              us.save()
-              redirect('')
-            
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            a=1
     else:
         form=RegisterForm()
             
