084fa6c580e6824c93f5f5d70028ad1ad32481d9
AitorElorza <aitor.elorza.segurola@gmail.com>
2022-04-27 18:49:29 +0200

    Register almost working

--- djangoLab/filmenGunea/forms.py
+++ djangoLab/filmenGunea/forms.py
@@ -1,10 +1,21 @@
 
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class RegisterForm(forms.Form):
     user= forms.CharField(label='Izena',max_length=20,required=True)
     pass1=forms.CharField(label='Pasahitza',widget=forms.PasswordInput, required=True)
     pass2=forms.CharField(label='Errepikatu pasahitza',widget=forms.PasswordInput, required=True)
+    class Meta:
+        model = User
+        fileds=("user","pass1","pass2")
+    
+    def save(self, commit=True):
+        user= super(RegisterForm, self).save(commit=False)
+        if commit:
+            user.save()
+        return user
 
 class LoginForm(forms.Form):
     user = forms.CharField(label='Izena',max_length=20,required=True)
--- djangoLab/filmenGunea/views.py
+++ djangoLab/filmenGunea/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from .forms import RegisterForm, LoginForm
 
 
@@ -7,14 +8,39 @@ def index(request):
     return render(request, 'filmenGunea/index.html')
 
 def register(request):
+
+    if request.method=='GET':
+        form=RegisterForm()    
+        return render(request, 'filmenGunea/register.html',{'form':form})
+    
     if request.method=='POST':
+        error=False
+        errorea=""
         form=RegisterForm(request.POST)
         if form.is_valid():
-            a=1
-    else:
-        form=RegisterForm()
-            
-    return render(request, 'filmenGunea/register.html',{'form':form})
+            user = request.POST['user']
+
+            #existizen da
+            if User.objects.filter(username=request.POST['user']).count() >0:
+                error=True
+                errorea="erabiltzailea existitzen da"
+                return render(request, 'filmenGunea/register.html',{'form':form, 'errorea':errorea,'error':error})
+            else:
+                pass1=request.POST['pass1']
+                pass2=request.POST['pass2']
+                #pasahitzak berdinak direla bermatu
+                if pass1 != pass2:
+                    #ez badira berdinak
+                    error=True
+                    errorea="Sartu dituzun pasahitzak ez dira berdinak"
+                    return render(request, 'filmenGunea/register.html',{'form':form, 'errorea':errorea,'error':error})
+                else:
+                    #berdinak badira
+                    User.objects.create_user(user,email=None, password=pass1)
+                    form = LoginForm()
+                    return render(request, 'filmenGunea/login.html',{'form': form})
+    
+    
 
 def login(request):
     if request.method=='POST':
@@ -23,8 +49,7 @@ def login(request):
             a=1
     else:
         form=LoginForm()
-    
-    return render(request, 'filmenGunea/login.html',{'form':form})
+        return render(request, 'filmenGunea/login.html',{'form':form})
 
 def filmakIkusi(request):
     return render(request, 'filmenGunea/filmakIkusi.html')
